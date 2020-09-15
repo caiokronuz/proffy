005e1d93dd18f1f0c76bfa3376c77e1d856e82b5
caiokronuz <caiogabriel135@gmail.com>
2020-09-15 12:24:59 -0300

    mudanças nas rotas e pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 import Landing from './pages/Landing';
@@ -8,8 +7,7 @@ import Landing from './pages/Landing';
 function Routes(){
     return(
         <BrowserRouter>
-            <Route path="/" exact component={LoginPage} />
-            <Route path="/landing" component={Landing} />
+            <Route path="/" exact component={Landing} />
             <Route path="/study" component={TeacherList} />
             <Route path="/give-classes" component={TeacherForm} />
         </BrowserRouter>
